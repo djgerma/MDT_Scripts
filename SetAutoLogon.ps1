1d65c26c2aba53673f0fb26916358b1d4f4eb0ce
djgerma <78435934+djgerma@users.noreply.github.com>
2021-09-28 16:37:08 -0400

    Update SetAutoLogon.ps1
    Added DefaultDomainName as an option

--- SetAutoLogon.ps1
+++ SetAutoLogon.ps1
@@ -1,6 +1,8 @@
 $RegPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
 $DefaultUsername = "UsernameHere"
 $DefaultPassword = "PasswordHere"
-Set-ItemProperty $RegPath "AutoAdminLogon" -Value "1" -type String 
+$DefaultDomainName = "FQDN Domain name Here" #If local admin acct use "."
+Set-ItemProperty $RegPath "AutoAdminLogon" -Value "1" -type String
+Set-ItemProperty $RegPath "DefaultDomainName" -Value $DefaultDomainName -type String
 Set-ItemProperty $RegPath "DefaultUsername" -Value "$DefaultUsername" -type String 
 Set-ItemProperty $RegPath "DefaultPassword" -Value "$DefaultPassword" -type String
