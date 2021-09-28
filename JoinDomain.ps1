6f6ec8041dd085b47043789b8a88299507aef1e8
djgerma <78435934+djgerma@users.noreply.github.com>
2021-09-28 16:50:15 -0400

    Update JoinDomain.ps1
    Added a part to sense if computer is already on the domain. If so MDT reboot step will query Registry and if the NOJOIN is there, it will skip rebooting task.

--- JoinDomain.ps1
+++ JoinDomain.ps1
@@ -1,3 +1,10 @@
+if ((gwmi win32_computersystem).partofdomain -eq $true) {
+    $Object = new-object -comobject wscript.shell
+    $Message = $Object.popup("This computer is already part of the Domain. Domain Join step will be skipped. ",5," Domain Join Step Warning ",1)
+    New-Item -Path "HKLM:\SOFTWARE" -Name NOJOIN
+    New-ItemProperty -Path "HKLM:\SOFTWARE\NOJOIN" -Name "NOJOIN" -Value "NOJOIN" -PropertyType "String"
+    Exit
+} Else {
 $domain = "domain name here"
 $password = Get-Content C:\FolderPath\JoinDomain.txt | ConvertTo-SecureString -Key (Get-Content 'C:\FolderPath\JoinDomain.key')
 $username = "$domain\UserNameHere" 
@@ -5,4 +12,5 @@ $credential = New-Object System.Management.Automation.PSCredential($username,$pa
 Add-Computer -DomainName $domain -Credential $credential
 
 Start-Sleep -s 5
-Get-ChildItem -Path 'C:\FolderPath\Temp' JoinDomain.* | foreach { Remove-Item -Path $_.FullName }
+Get-ChildItem -Path 'C:\FolderPath' JoinDomain.* | foreach { Remove-Item -Path $_.FullName }
+}
