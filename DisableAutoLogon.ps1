aaf2e27cbefd690facd266e84efea4a8abf81ac4
djgerma <78435934+djgerma@users.noreply.github.com>
2021-09-28 16:34:32 -0400

    Update DisableAutoLogon.ps1
    Added DefaultDomainName as part to clean up as well

--- DisableAutoLogon.ps1
+++ DisableAutoLogon.ps1
@@ -1,6 +1,8 @@
 $RegPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
-$DefaultUsername = "UsernameHere"
+$DefaultUsername = "Administrator"
 $DefaultPassword = ""
-Set-ItemProperty $RegPath "AutoAdminLogon" -Value "0" -type String 
+$DefaultDomainName = ""
+Set-ItemProperty $RegPath "AutoAdminLogon" -Value "0" -type String
+Set-ItemProperty $RegPath "DefaultDomainName" -Value $DefaultDomainName -type String 
 Set-ItemProperty $RegPath "DefaultUsername" -Value "$DefaultUsername" -type String 
 Set-ItemProperty $RegPath "DefaultPassword" -Value "$DefaultPassword" -type String
