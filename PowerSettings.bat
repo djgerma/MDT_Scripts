a6eee023d8f73cf798324d6e1610d6ff36a93ef2
djgerma <78435934+djgerma@users.noreply.github.com>
2021-09-28 17:02:20 -0400

    Create PowerSettings.bat
    This will change power settings to Ultimate Performance which will speed MDT Deployment up. We can add this right at the beginning of the deployment to make sure entire process goes faster. This can also be added even when doing an entire PXE OS deployment, it will shave off quite a bit of time.

---
+++ PowerSettings.bat
@@ -0,0 +1,3 @@
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\238C9FA8-0AAD-41ED-83F4-97BE242C8F20\7bc4a2f9-d8fc-4469-b07b-33eb785aaca0" /v Attributes /t REG_DWORD /d 2 /f
+
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\7516b95f-f776-4464-8c53-06167f40cc99\8EC4B3A5-6868-48c2-BE75-4F3044BE88A7" /v Attributes /t REG_DWORD /d 2 /f
